76d3b71ca32b89967b8c1b483368e57348fa4e13
guistetter <guilherme_stetter@hotmail.com>
2020-08-11 15:39:30 -0300

    criando middleware para area restrito, aplicando session-express

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const mongo = process.env.MONGODB || 'mongodb://localhost/autenticacao-autorizacao'
+
 const User = require('./models/user')
 const noticias = require('./routes/noticias')
 const restrito = require('./routes/restrito')
+
 mongoose.Promise = global.Promise
 
 app.set('views', path.join(__dirname, 'views'))
@@ -14,10 +17,24 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+app.use(session({secret: 'meu-segredo'}))
+//interceptar usuario logado ou nao no middleware
+app.use('/restrito',(req,res, next) => {
+  console.log('opa')
+  if('user' in req.session){
+    return next()
+  }
+  res.redirect('/login') 
+})
+
 app.use('/restrito', restrito)
 app.use('/noticias', noticias)
 
 app.get('/', (req, res) => res.render('index'))
+
+app.get('/login', (req,res) =>{
+  res.render('login')
+})
 
 const createInitialUser = async() => {
   const total = await User.count({username: 'gui'})
