7762e8d568814988cf0518ea33298f9eae89e08a
guistetter <guilherme_stetter@hotmail.com>
2020-08-11 16:46:17 -0300

    metodo para validar senha do usuario

--- models/user.js
+++ models/user.js
@@ -28,6 +28,18 @@ UserSchema.pre('save',function(next){
     })
   })
 })
+//checar senha do usuario
+UserSchema.methods.checkPassword = function(password){
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password,(err, isMatch) => {
+      if(err){
+        reject(err)
+      } else {
+        resolve(isMatch)
+      }
+    })
+  })
+}
 
 const User = mongoose.model('User', UserSchema)
 module.exports = User
