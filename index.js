747eda1a70767b511be49b5ad4b6a5c989286bd7
guistetter <guilherme_stetter@hotmail.com>
2020-08-11 14:47:09 -0300

    Metodo para criar usuario inicial 'root', se existir nao criar

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const User = require('./models/user')
+
 mongoose.Promise = global.Promise
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/autenticacao-autorizacao'
@@ -14,15 +16,24 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'))
 
+const createInitialUser = async() => {
+  const total = await User.count({username: 'gui'})
+  if( total === 0){
+    const user = new User({
+      username: 'gui',
+      password: 'abc123'
+      })
+    await user.save()
+    console.log('Usuario root criado')
+  }else {
+    console.log('Usuario root já existe, criação n é necessario')
+  }
+}
+
 mongoose.connect(mongo, {useMongoClient: true})
   .then(()=>{
+    createInitialUser()
     app.listen(port, () => console.log('listening...'))
   })
   .catch(e => console.log(e))
 
-const User = require('./models/user')
-const user = new User({
- username: 'gui',
- password: 'abc123'
-})
-user.save(()=> console.log('salvou a senha'))
