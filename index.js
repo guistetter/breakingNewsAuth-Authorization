bca5a1e75b788079464940448b583c3a102bcdb9
guistetter <guilherme_stetter@hotmail.com>
2020-08-11 16:47:39 -0300

    rota login, metodo para verificar senha do usuario, permiter acesso a area restrita

--- index.js
+++ index.js
@@ -30,7 +30,13 @@ app.use('/restrito',(req,res, next) => {
 
 app.post('/login', async (req,res) => {
   const user = await User.findOne({username: req.body.username})
-  res.send(user)
+  const isValid = await user.checkPassword(req.body.password)
+  if(isValid){
+    req.session.user = user
+    res.redirect('/restrito/noticias')
+  } else {
+    res.redirect('/login')
+  }
 })
 
 app.use('/restrito', restrito)
