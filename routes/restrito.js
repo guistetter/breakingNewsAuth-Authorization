4f530fc18bd21f7224630148e502305aeab5bd99
guistetter <guilherme_stetter@hotmail.com>
2020-08-11 15:40:16 -0300

    organizando rotas area restrita de noticias

--- routes/restrito.js
+++ routes/restrito.js
@@ -3,5 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => res.send('restrito'))
+router.get('/noticias', (req, res) => res.send('noticias restritas'))
 
 module.exports = router
