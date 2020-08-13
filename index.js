5e1e34a7aedea1f7976a527142bed00a1767d4a4
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 15:54:43 -0300

    criando noticias fakes apenas para teste, inicia servidor criando 1noticia publica e 1 noticia privada

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const session = require('express-session')
 const app = express()
 const path = require('path')
 const bodyParser = require("body-parser")
-const port = process.env.PORT || 3001
 const mongoose = require('mongoose')
+
 const mongo = process.env.MONGODB || 'mongodb://localhost/autenticacao-autorizacao'
+const port = process.env.PORT || 3001
+mongoose.Promise = global.Promise
 
 const User = require('./models/user')
+const Noticia = require('./models/noticia')
 
 const noticias = require('./routes/noticias')
 const restrito = require('./routes/restrito')
 const auth = require('./routes/auth')
 const pages = require('./routes/pages')
 
-mongoose.Promise = global.Promise
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -48,16 +50,31 @@ app.use('/', pages)
 
 const createInitialUser = async() => {
   const total = await User.count({username: 'gui'})
+
   if( total === 0){
     const user = new User({
       username: 'gui',
       password: 'abc123'
       })
+
     await user.save()
     console.log('Usuario root criado')
   }else {
     console.log('Usuario root já existe, criação n é necessario')
   }
+  const noticia = new Noticia({
+    title: "Notícia Pública" +new Date().getTime(),
+    content: 'content',
+    category: 'public'
+  })
+  await noticia.save()
+
+  const noticia2 = new Noticia({
+    title: "Notícia Privada" +new Date().getTime(),
+    content: 'content',
+    category: 'private'
+  })
+  await noticia2.save()
 }
 
 mongoose.connect(mongo, {useMongoClient: true})
