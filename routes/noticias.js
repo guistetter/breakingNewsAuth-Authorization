3e2bbf667340c6140c08920406d7103d6415872a
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 15:46:27 -0300

    pegando todas as notificas do banco de dados e passando pra rota noticias/index

--- routes/noticias.js
+++ routes/noticias.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const Noticia = require('../models/noticia')
 
-router.get('/', (req, res) => {
-  res.send('noticias publicas')
+router.get('/', async (req, res) => {
+  const noticias = await Noticia.find({})
+  res.render('noticias/index',{noticias})
 })
 
 module.exports = router
