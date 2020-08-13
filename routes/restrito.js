f127e1f7f1ae2a0133ffe1a9287b03d72f635447
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 16:09:11 -0300

    filtrando noticias privadas com o banco e passando pra rota

--- routes/restrito.js
+++ routes/restrito.js
@@ -2,7 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.send('restrito'))
-router.get('/noticias', (req, res) => res.send('noticias restritas'))
+const Noticia = require('../models/noticia'
+)
+router.get('/', (req, res) => {
+  res.send('restrito')
+})
+//nao preciso validar usuario, pois o middleware já garante que se o usuario
+//chegou aqui ele já ta logado, diferente das noticias publicas...
+router.get('/noticias', async (req, res) => {
+  const noticias = await Noticia.find({category: 'private'})
+  res.render('noticias/restrito', {noticias})
+})
 
 module.exports = router
