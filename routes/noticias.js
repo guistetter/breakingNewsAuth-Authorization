129c0d305d97722ab3c3eabf7549ebb14457b28e
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 16:08:26 -0300

    passando noticias publicas pra rota

--- routes/noticias.js
+++ routes/noticias.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const Noticia = require('../models/noticia')
 
 router.get('/', async (req, res) => {
-  let conditions = {}
-  if(!('user' in req.session)){
-    conditions = {category:"public"}
-  }
+  /*desta forma usuario nao logado ve apenas publica, usuario logado ve tudo
+   let conditions = {}
+   if(!('user' in req.session)){
+     conditions = {category:"public"}
+   }*/ 
+  const conditions = {category: 'public'}
   const noticias = await Noticia.find(conditions)
   res.render('noticias/index',{noticias})
 })
