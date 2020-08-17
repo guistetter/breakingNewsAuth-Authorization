055c0ff08831f9ea147fdbff57ada1a24d0c867c
guistetter <guilherme_stetter@hotmail.com>
2020-08-17 13:57:26 -0300

    definindo e separando rota para usuario de nivel admin

---
+++ routes/admin.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const Noticia = require('../models/noticia')
+
+//interceptar usuario logado ou nao no middleware
+router.use((req,res, next) => {
+  console.log('opa')
+  if('user' in req.session){
+    if(req.session.user.roles.indexOf('admin')>=0)
+    {
+      return next()
+    }else{
+      res.redirect('/')
+    }
+  }
+  res.redirect('/login') 
+})
+
+router.get('/', (req, res) => {
+  res.send('admin')
+})
+
+router.get('/noticias', async (req, res) => {
+  const noticias = await Noticia.find({})
+  res.render('noticias/admin', {noticias})
+})
+
+module.exports = router
