b79d76c0097cc935f6d3407f425174121e44d4a1
guistetter <guilherme_stetter@hotmail.com>
2020-08-18 15:36:21 -0300

    adicionando autenticacao com google

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = require('../models/user')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 
 router.use(passport.initialize())
 router.use(passport.session())
@@ -52,6 +53,27 @@ passport.use(new FacebookStrategy({
   }
 }))
 
+//Strategia com google
+passport.use(new GoogleStrategy({
+  clientID:'528487486853-8sgll4252bofj6k331e4hotbrg259191.apps.googleusercontent.com',
+  clientSecret:'AIzaSyASy7DCm2pbfbax6jRDHOKdF4K5EekPhf8',
+  callbackURL:'http://localhost:3000/google/callback',
+},async(accessToken, refreshToken, err , profile, done)=>{
+  const userDB = await User.findOne({googleId: profile.id})
+  if(!userDB){
+    const user = new User({
+      name: profile.displayName,
+      googleId: profile.id,
+      roles:['restrito']
+    })
+    await user.save()
+    done(null,user)
+  } else{
+    done(null, userDB)
+  }
+}))
+
+
 //middleware para mosrar usuario logado
 router.use((req,res, next) => {
   if(req.isAuthenticated()){
@@ -89,11 +111,17 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/login',
   failureFlash: false
 }))
+
 router.get('/facebook', passport.authenticate('facebook'))
 router.get('/facebook/callback',
   passport.authenticate('facebook',{failureRedirect:'/'}),
   (req,res) =>{
     res.redirect('/')
   }
+)
+
+router.get('/google', passport.authenticate('google',{ scope:['https://www.googleapis.com/auth/userinfo.profile']}))
+router.get('/google/callback',
+  passport.authenticate('google',{failureRedirect:'/', successRedirect:"/"}),
 )
 module.exports = router
